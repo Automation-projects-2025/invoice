4947bc0ce182158fd908ff2e47bfccd09adfa265
Automation-projects-2025 <projectsautomation9@gmail.com>
2025-04-02 16:46:29 +0530

    Update build.sh
    seven

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-# Install Tesseract and Poppler
+# Install system packages needed for PDF OCR
 apt-get update && apt-get install -y \
     tesseract-ocr \
-    poppler-utils
+    poppler-utils \
+    ghostscript \
+    libgl1 \
+    fonts-dejavu-core
 
-# Install Python dependencies
+# Install Python packages
+pip install --upgrade pip
 pip install -r requirements.txt
